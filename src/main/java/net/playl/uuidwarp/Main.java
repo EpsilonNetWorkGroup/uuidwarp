f9049d90c7d1ba017eaf499f5290c585727b2221
Dmitri <36127740+Dmitri233@users.noreply.github.com>
2022-09-25 14:12:20 +0800

    Fix Error

--- src/main/java/net/playl/uuidwarp/Main.java
+++ src/main/java/net/playl/uuidwarp/Main.java
@@ -14,29 +14,37 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 public class Main extends JavaPlugin {
-    private static final Path advancements = Bukkit.getWorld("world").getWorldFolder().toPath().resolve("advancements");
-    private static final Path playerdata = Bukkit.getWorld("world").getWorldFolder().toPath().resolve("playerdata");
-    private static final Path stats = Bukkit.getWorld("world").getWorldFolder().toPath().resolve("stats");
 
     public void onEnable() {
-        Bukkit.broadcast(Component.text("重要: 正在修改全部玩家文件名称UUID为盗版!!!", NamedTextColor.RED));
-        Arrays.asList(Bukkit.getOfflinePlayers()).forEach(p -> {
-            Component userName = Component.text(p.getName(), NamedTextColor.GOLD);
-            String uuid = p.getUniqueId().toString();
-            String offlineUuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + p.getName()).getBytes(StandardCharsets.UTF_8)).toString();
-            Bukkit.broadcast(Component.text("正在修改玩家: ", NamedTextColor.YELLOW).append(userName).append(Component.text("原UUID: " + uuid)));
-            try {
-                Bukkit.broadcast(Component.text("状态(adv, playerdata, playerdataold, stats): "
-                        + Files.move(advancements.resolve(uuid + ".json"), advancements.resolve(offlineUuid + ".json"))
-                        + Files.move(playerdata.resolve(uuid + ".dat"), playerdata.resolve(offlineUuid + ".dat"))
-                        + Files.move(playerdata.resolve(uuid + ".dat_old"), playerdata.resolve(offlineUuid + ".dat_old"))
-                        + Files.move(stats.resolve(uuid + ".json"), stats.resolve(offlineUuid + ".json"))));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        Bukkit.broadcast(Component.text("现已完成UUID的更改, 服务器关闭!!!", NamedTextColor.GREEN));
-        Bukkit.getServer().shutdown();
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            final Path advancements = Bukkit.getWorld("world").getWorldFolder().toPath().resolve("advancements");
+            final Path playerdata = Bukkit.getWorld("world").getWorldFolder().toPath().resolve("playerdata");
+            final Path stats = Bukkit.getWorld("world").getWorldFolder().toPath().resolve("stats");
+            Bukkit.broadcast(Component.text("重要: 正在修改全部玩家文件名称UUID为盗版!!!", NamedTextColor.RED));
+            Arrays.stream(Bukkit.getOfflinePlayers()).forEach(p -> {
+                Component userName;
+                try {
+                userName = Component.text(p.getName(), NamedTextColor.GOLD);
+                } catch (NullPointerException e) {
+                    Bukkit.broadcast(Component.text("错误, 没有用户名缓存: "+p.getUniqueId(), NamedTextColor.RED));
+                    return;
+                }
+                String uuid = p.getUniqueId().toString();
+                String offlineUuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + p.getName()).getBytes(StandardCharsets.UTF_8)).toString();
+                Bukkit.broadcast(Component.text("正在修改玩家: ", NamedTextColor.YELLOW).append(userName).append(Component.text("原UUID: " + uuid)));
+                try {
+                    Bukkit.broadcast(Component.text("状态(adv, playerdata, playerdataold, stats): "
+                            + Files.move(advancements.resolve(uuid + ".json"), advancements.resolve(offlineUuid + ".json"))
+                            + Files.move(playerdata.resolve(uuid + ".dat"), playerdata.resolve(offlineUuid + ".dat"))
+                            + Files.move(playerdata.resolve(uuid + ".dat_old"), playerdata.resolve(offlineUuid + ".dat_old"))
+                            + Files.move(stats.resolve(uuid + ".json"), stats.resolve(offlineUuid + ".json"))));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            Bukkit.broadcast(Component.text("现已完成UUID的更改, 服务器关闭!!!", NamedTextColor.GREEN));
+            Bukkit.getServer().shutdown();
+        }, 1200L);
     }
 
     public void onDisable() {
